5e09c819fc26c78fbd13bef2a3af39e1ebb0978d
bmehler <bernhard.mehler@gmail.com>
2015-09-03 08:33:37 +0000

    changed style

--- public/module/video/filter/TimestampFilter.js
+++ public/module/video/filter/TimestampFilter.js
@@ -17,36 +17,36 @@ function TimestampFilter () {
       if (regex1.test(item)) {
         var a = item.replace(/M/, '');
         if (a.length < 2) {
-        return '00:0' + a + ':00';
-      } else {
-        return '00:' + a + ':00';
+          return '00:0' + a + ':00';
+        } else {
+          return '00:' + a + ':00';
+        }
+      }
+      if (regex2.test(item)) {
+        var b = item.replace(/S/, '');
+        if (b.length < 2) {
+          return '00:0' + b;
+        } else {
+          return '00:' + b;
       }
     }
-    if (regex2.test(item)) {
-      var b = item.replace(/S/, '');
-      if (b.length < 2) {
-      return '00:0' + b;
-    } else {
-      return '00:' + b;
-    }
-  }
     if (regex3.test(item)) {
       var c = item.replace(/H/, '');
       if (c.length < 2) {
-      return '0' + c + ':00:00';
-    } else {
-      return c + ':00:00';
+        return '0' + c + ':00:00';
+      } else {
+        return c + ':00:00';
+      }
     }
-  }
-  return duration.toString();
-  });
-  } else {
-    var tstamp = item.match(/(\d+)(?=[MHS])/ig) || [];
-    var without = tstamp.map(function (item) {
-      if (item.length < 2) { return '0' + item; }
-        return item;
-      }).join(':');
-      return without.toString();
+    return duration.toString();
+    });
+    } else {
+      var tstamp = item.match(/(\d+)(?=[MHS])/ig) || [];
+      var without = tstamp.map(function (item) {
+        if (item.length < 2) { return '0' + item; }
+          return item;
+        }).join(':');
+        return without.toString();
     }
   };
 }
